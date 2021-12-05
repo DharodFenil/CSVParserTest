4d24a43b543cda73c80bef5408e6ddf89c0aadf0
DharodFenil <dharodfenil@gmail.com>
2021-12-05 22:31:13 +0530

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSVParseTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("CSVParseTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("416c65e0-893a-4517-81f1-7f389099bb0a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest.cs
@@ -0,0 +1,64 @@
+﻿using CSVParse.Classes;
+using CSVParse.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace CSVParseTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        /// <summary>
+        /// Test case with record having no value for Price Multiplier in the Algo Params column
+        /// Checks if the all records have read successfully
+        /// </summary>
+        [TestMethod]
+        public void TestMethod_NoPriceMultiplierValue()
+        {
+            int rowCount = 0;
+            CSVParser parse = new CSVParser();
+            List<OutputCSVModel> lst = parse.Read(@"C:\Users\Fenil\source\repos\CSVParse\Files\DataExtractor_Example_Input_NoContractSize.csv", "DataExtractor_Example_Input_NoContractSize.csv", ref rowCount);
+            Assert.IsTrue(lst.Count > 0 && lst.Count == rowCount); //Number of records in the source file should be >0 and same as the records in the file to be written.
+        }
+
+        /// <summary>
+        /// Test case with correct data -- Ideal case
+        /// Checks if the all records have read successfully
+        /// </summary>
+        [TestMethod]
+        public void TestMethod_Ideal()
+        {
+            int rowCount = 0;
+            CSVParser parse = new CSVParser();
+            List<OutputCSVModel> lst = parse.Read(@"C:\Users\Fenil\source\repos\CSVParse\Files\DataExtractor_Example_Input.csv", "DataExtractor_Example_Input.csv", ref rowCount);
+            Assert.IsTrue(lst.Count > 0 && lst.Count == rowCount);//Number of records in the source file should be >0 and same as the records in the file to be written.
+        }
+
+        /// <summary>
+        /// Test case for file which are not CSV
+        /// Checks if the all records have read successfully
+        /// </summary>
+        [TestMethod]
+        public void TestMethod_WrongExtension()
+        {
+            int rowCount = 0;
+            CSVParser parse = new CSVParser();
+            List<OutputCSVModel> lst = parse.Read(@"C:\Users\Fenil\source\repos\CSVParse\Files\DataExtractor.docx", "DataExtractor.docx", ref rowCount);
+            Assert.IsTrue(lst.Count > 0 && lst.Count == rowCount);//Number of records in the source file should be >0 and same as the records in the file to be written.
+        }
+
+        /// <summary>
+        /// Test case with missing ISIN column in the CSV file
+        /// Checks if the all records have read successfully
+        /// </summary>
+        [TestMethod]
+        public void TestMethod_MissingColumn()
+        {
+            int rowCount = 0;
+            CSVParser parse = new CSVParser();
+            List<OutputCSVModel> lst = parse.Read(@"C:\Users\Fenil\source\repos\CSVParse\Files\DataExtractor_Example_Input_NoISIN.csv", "DataExtractor_Example_Input_NoISIN.csv", ref rowCount);
+            Assert.IsTrue(lst.Count > 0 && lst.Count == rowCount);//Number of records in the source file should be >0 and same as the records in the file to be written.
+        }
+    }
+}
